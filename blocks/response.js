cb5a419470f4ad071abcf6d3c095b138dece1c9b
Veikko Salminen <veikko.salminen@visma.com>
2018-12-18 10:06:40 +0200

    block response: support for cors headers

--- blocks/response.js
+++ blocks/response.js
@@ -9,7 +9,15 @@ class _block extends block {
         const body = this.exists("body") ? this.get("body", null) : {message: this.get("message", null)}
         if (this.get("meta", false)) body.meta = this.blocks.logger.getLog()
 
-        return {statusCode: this.get("statusCode", 200), body: JSON.stringify(body), headers: this.get("headers", {})}
+        const headers = this.get("headers", {})
+
+        if (this.get("cors", false)) {
+            headers["Access-Control-Allow-Credentials"] = true
+            headers["Access-Control-Allow-Origin"] = "*"
+            headers["Content-Type"] = "application/json"
+        }
+
+        return {statusCode: this.get("statusCode", 200), body: JSON.stringify(body), headers: headers}
     }
 }
 
